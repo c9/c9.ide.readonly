fe86d1f1528d960568fda2834d2273b8b5ed9ed0
nightwing <amirjanyan@gmail.com>
2016-02-18 10:40:31 +0400

    fix indentation

--- access_control.js
+++ access_control.js
@@ -106,42 +106,42 @@ define(function(require, exports, module) {
                 return showCancelAccessDialog();
             
             showQuestion("Workspace Access",
-              "You don't currently have " + (write ? "write " : "")
-              + "access to this workspace",
-              "Would you like to request access?",
-              function(){
-                  // Yes
-                 requestAccess();
-                 lastInfo.pending = true;
-              },
-              redirectToDashboardIfPrivate,
-              {
-                  yes: "Request access",
-                  no: "Not now"
-              }
+                "You don't currently have " + (write ? "write " : "")
+                + "access to this workspace",
+                "Would you like to request access?",
+                function(){
+                    // Yes
+                   requestAccess();
+                   lastInfo.pending = true;
+                },
+                redirectToDashboardIfPrivate,
+                {
+                    yes: "Request access",
+                    no: "Not now"
+                }
             );
         }
 
         function requestAccess() {
-             api.collab.post("request_access", function (err, member) {
-                 if (err) return showAlert("Error Requesting access", err.message || err);
-                 showAlert("Done", "Access request sent", "We have sent an access request to the admin of this workspace.");
-             });
+            api.collab.post("request_access", function (err, member) {
+                if (err) return showAlert("Error Requesting access", err.message || err);
+                showAlert("Done", "Access request sent", "We have sent an access request to the admin of this workspace.");
+            });
         }
 
         function showCancelAccessDialog() {
             showQuestion("Workspace Access",
-              "Request access pending approval",
-              "Would you like to cancel your access request?",
-              function(){
-                  // Yes
-                  api.collab.delete("cancel_request", function (err) {
-                      if (err) return showAlert("Error", err);
-                      lastInfo.pending = false;
-                      showAlert("Done", "Access request cancelled", "We don't currently have access to this workspace");
-                  });
-              },
-              redirectToDashboardIfPrivate
+                "Request access pending approval",
+                "Would you like to cancel your access request?",
+                function(){
+                    // Yes
+                    api.collab.delete("cancel_request", function (err) {
+                        if (err) return showAlert("Error", err);
+                        lastInfo.pending = false;
+                        showAlert("Done", "Access request cancelled", "We don't currently have access to this workspace");
+                    });
+                },
+                redirectToDashboardIfPrivate
             );
         }
 
